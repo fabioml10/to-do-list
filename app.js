df2f334e040dbc0766626de9b003b15011fc4b73
FABIO MULLER <fabioml10@yahoo.com.br>
2020-04-29 17:07:14 -0300

    chain of responsability

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require('express')
 
 const app = express();
 
+//middleware que manda json para o express
+app.use(express.json())
+
+const log = (req, res, next) => {
+  console.log(req.body)
+  console.log(`Data: ${Date.now()}`)
+  next()
+}
+
+app.use(log)
+
 app.get('/', (req, res) => {
   res.send("<h1>ToDo List</h1>")
+})
+
+app.get('/json', (req, res) => {
+  res.json({ title: "TarefaX", done: true })
 })
 
 app.listen(3000, () => {
---
+++ readme.md
@@ -0,0 +1,1 @@
+Todo lista created with NodeJS.
